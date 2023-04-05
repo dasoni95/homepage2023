70438ff93922bb548ebebd94db12517f6da1757f
YUJIN <dasoni95@kakao.com>
2023-04-05 17:29:56 +0900

    230405

---
+++ homepage2023/src/main/java/egovframework/let/temp/service/TempVO.java
@@ -0,0 +1,32 @@
+package egovframework.let.temp.service;
+
+import java.io.Serializable;
+
+import egovframework.com.cmm.ComDefaultVO;
+
+public class TempVO extends ComDefaultVO implements Serializable {
+
+	
+	//임시데이터 ID
+	private String tempId;
+	
+	public String getTempId() {
+		return tempId;
+	}
+
+	public void setTempId(String tempId) {
+		this.tempId = tempId;
+	}
+
+	public String getTempVal() {
+		return tempVal;
+	}
+
+	public void setTempVal(String tempVal) {
+		this.tempVal = tempVal;
+	}
+
+	//임시데이터값
+	private String tempVal;
+	
+}
